a5e487dff704cc73892d7491050e86566542a9d8
zlatan <zlatan.pham@gmail.com>
2020-10-15 01:25:37 +0700

    share tailwind config

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -1,32 +1,3 @@
-const tailwindUI = require('@tailwindcss/ui');
-const truncateMultiLine = require("tailwindcss-truncate-multiline")();
+const { tailwindConfig } = require('sajari-react-sdk-styles');
 
-module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    truncate: {
-      lines: {
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-      }
-    },
-    extend: {
-      fontSize: {
-        'code-inline': '0.9375em',
-      }
-    }
-  },
-  variants: {
-    boxShadow: ['responsive', 'hover', 'focus', 'focus-within'],
-    textColor: ['responsive', 'hover', 'focus', 'group-hover', 'group-focus'],
-    backgroundColor: ['responsive', 'hover', 'focus', 'checked', 'group-hover', 'group-focus'],
-    borderColor: ['responsive', 'hover', 'focus', 'checked'],
-  },
-  plugins: [tailwindUI, truncateMultiLine],
-};
+module.exports = tailwindConfig;
